5f171d269deca2ce10396f11bd61c7af8e6af1bf
Abduino <abdrehmanredi0@gmail.com>
2022-09-15 11:59:47 -0300

    Get Post and Request Methods

---
+++ basic/phpFormMethod.php
@@ -0,0 +1,58 @@
+<?php
+include "menu.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>html form</title>
+</head>
+<body>
+	<form action="" method="post">
+		F_name: <input type="text" name="f_name"><br><br>
+		L_name: <input type="text" name="l_name"><br><br>
+		<input type="submit" name="submit">
+	</form>
+</body>
+</html>
+<?php
+/*
+if (isset($_GET['submit'])) {
+	echo "<br> the form data recived<br>";
+	echo "the first name is : " . $_GET['f_name'] ."<br>";
+	echo "the last name is : " . $_GET['l_name'];
+	// code...
+}
+*/
+/*
+if (isset($_POST['submit'])) {
+	echo "<br> the form data recived<br>";
+	echo "the first name is : " . $_POST['f_name'] ."<br>";
+	echo "the last name is : " . $_POST['l_name'];
+	// code...
+}
+*/
+if (isset($_REQUEST['submit'])) {
+	echo "<br> the form data recived<br>";
+	echo "the first name is : " . $_REQUEST['f_nmae']."<br>";
+	echo "the last name is : " . $_REQUEST['l_name'];
+	// code...
+}
+
+
+
+/*
+echo "<br>";
+include "menu.php";
+echo "<br><br>--------------------------<br>";
+echo "<form action='' method=''>
+		F_name: <input type='text' name='f_name'><br><br>
+		L_name: <input type='text' name='l_name'><br><br>
+		<input type='submit' name='submit'>
+	 </form>";
+
+*/
+
+include "footer.php";
+?>
