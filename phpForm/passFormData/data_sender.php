14c865ac4417d091193fbc8d6e6d7d8f8c984409
Abduino <abdrehmanredi0@gmail.com>
2022-10-08 16:49:28 -0300

    receiver file added

---
+++ phpForm/passFormData/data_sender.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>php form</title>
+</head>
+<body>
+	<form method="get" action="data_reciver.php">
+		First name: <input type="text" name="f_name"><br>
+		Last name: <input type="text" name="l_name"><br>
+		<input type="submit" name="submit">
+	</form>
+</body>
+</html>
